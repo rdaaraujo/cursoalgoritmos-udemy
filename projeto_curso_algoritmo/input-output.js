0b717e6a45c609c6b03078ca6209d7077c98228b
Rafael Almeida <rafaelalmeidaaraujo@outlook.com>
2022-04-13 16:12:33 -0300

    commit curso algoritmos

---
+++ projeto_curso_algoritmo/input-output.js
@@ -0,0 +1,6 @@
+var nome, idade;
+
+nome = prompt("Digite o seu nome: ")
+idade = prompt("Digite a sua idade: ")
+
+alert("Seu nome e " + nome + " e voce tem "+ idade + " anos, certo?")
---
+++ projeto_curso_algoritmo/primeiro.js
@@ -0,0 +1,14 @@
+var nota, nota2, media, nome, primeiroNome, segundoNome, idade, endereco; 
+
+// Comentário de linha
+
+/*Comentário
+
+de
+
+bloco*/
+
+nota = 100
+nota2 = 150
+media = nota2 - nota
+alert ("Resultado da Nota: " + media);
